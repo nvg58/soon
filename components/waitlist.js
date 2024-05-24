3a2d6250e6b792460b0b67d1a88b7d5825320773
Giap Nguyen <nvg58@users.noreply.github.com>
2024-05-24 11:11:25 +0000

    Update logo

--- components/waitlist.js
+++ components/waitlist.js
@@ -56,11 +56,9 @@ export default function Waitlist() {
     >
       <div className="flex flex-col text-base text-white">
         <div className="font-medium">Join the waitlist</div>
-        <div className="text-white text-opacity-50">Sign up to be one of the first to use Dex3 and get airdrop.</div>
+        <div className="text-white text-opacity-50">Sign up to be one of the first to use Dex3 and get airdrop 🪂</div>
       </div>
-      <button>
-        <a href="https://t.me/dex3fiann" className="inline-flex items-center justify-center px-5 py-2 bg-white rounded-full text-black text-base font-medium leading-6 select-none">Join us</a>
-      </button>      
+      <a href="https://t.me/dex3fiann" className="inline-flex items-center justify-center px-5 py-2 bg-white rounded-full text-black text-base font-medium leading-6 select-none">Join us</a>
       {/* <form onSubmit={handleSubmit}>
         <label className={style.waitlist__form}>
           <input
