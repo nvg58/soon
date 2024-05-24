8badb30f4d307939466936655c7cc63bce42da52
Giap Nguyen <nvg58@users.noreply.github.com>
2024-05-24 19:06:15 +0700

    Update icons.js

--- components/icons.js
+++ components/icons.js
@@ -99,8 +99,8 @@ export function Dribbble(props) {
 export function Discord(props) {
   return (
     <svg
-      width={20}
-      height={20}
+      width={25}
+      height={25}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
