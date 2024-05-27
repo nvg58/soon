1286e590dcb7febfe569076caceeebb93724745a
Giap Nguyen <nvg58@users.noreply.github.com>
2024-05-27 02:46:39 +0000

    Update page metadata

--- pages/_app.js
+++ pages/_app.js
@@ -25,23 +25,23 @@ function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover" />
 
-        <title>Dex3 - Hassle-free meme trading platform built for degen traders</title>
-        <meta name="title" content="Dex3 - Hassle-free meme trading platform built for degen traders" />
-        <meta name="description" content="Dex3 - Hassle-free meme trading platform built for degen traders. Built by degens for degens." />
+        <title>dex3.fi - A lighting-fast, hassle-free platform that turbocharges your meme trading adventure</title>
+        <meta name="title" content="dex3.fi - A lighting-fast, hassle-free platform that turbocharges your meme trading adventure" />
+        <meta name="description" content="dex3.fi - A lighting-fast, hassle-free platform that turbocharges your meme trading adventure. Crafted by degens, tailored for enthusiasts!" />
         <meta name="keywords" content="Web3, Decentralize, Revolution" />
 
         <meta property="og:site_name" content="Dex3" />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://dex3.fi" />
-        <meta property="og:title" content="Dex3 - Hassle-free meme trading platform built for degen traders" />
-        <meta property="og:description" content="Dex3 - Hassle-free meme trading platform built for degen traders. Built by degens for degens." />
+        <meta property="og:title" content="dex3.fi - A lighting-fast, hassle-free platform that turbocharges your meme trading adventure" />
+        <meta property="og:description" content="dex3.fi - A lighting-fast, hassle-free platform that turbocharges your meme trading adventure. Crafted by degens, tailored for enthusiasts!" />
         <meta property="og:image" content="/thumbnail.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://dex3.fi/" />
-        <meta property="twitter:title" content="Dex3 - Hassle-free meme trading platform built for degen traders" />
-        <meta property="twitter:description" content="Dex3 - Hassle-free meme trading platform built for degen traders. Built by degens for degens. The icons are carefully crafted for you to work seamlessly." />
+        <meta property="twitter:title" content="dex3.fi - A lighting-fast, hassle-free platform that turbocharges your meme trading adventure" />
+        <meta property="twitter:description" content="dex3.fi - A lighting-fast, hassle-free platform that turbocharges your meme trading adventure. Crafted by degens, tailored for enthusiasts!" />
         <meta property="twitter:image" content="/thumbnail.png" />
       </Head>
       <Component {...pageProps} />
